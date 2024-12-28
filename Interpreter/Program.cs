fef3e3e52b663bfab3d0077fc9bf3004c6d4e1b9
Praveen S R <praveentiger1997@gmail.com>
2024-12-28 14:49:34 +0530

    Completion of Behavioral patterns

---
+++ Interpreter/Program.cs
@@ -0,0 +1,16 @@
+﻿using Interpreter;
+
+var expressions = new List<RomanExpression>()
+{
+    new RomanHundredExpression(),
+    new RomanTenExpression(),
+    new RomanOneExpression()
+};
+int input = 975;
+var context = new RomanContext(input);
+
+foreach (var expression in expressions)
+    expression.Interpret(context);
+
+Console.WriteLine($"Translating Arabic numeral to Roman numeral: {input} = {context.Output}");
+Console.ReadKey();
